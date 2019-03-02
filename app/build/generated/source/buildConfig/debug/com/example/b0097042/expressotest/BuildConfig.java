14a9fc5bdb45a319401c127cad917390c313a4f9
abhinavoffice <abhinav.agarwal@wynk.in>
2019-03-02 11:41:29 +0530

    version change

--- app/build/generated/source/buildConfig/debug/com/example/b0097042/expressotest/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/b0097042/expressotest/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.example.b0097042.expressotest";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "3.1.0";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "4.0.0";
 }
